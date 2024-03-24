87c6f4b72a72ec04fb29bb83a9d7219180907622
PontusMartinsson <pontus.martinsson@elev.ga.ntig.se>
2024-03-24 15:52:29 +0100

    allt gemensamt färdigt

--- js/main.js
+++ js/main.js
@@ -10,12 +10,25 @@ function expand() {
 
 
 var minimizeButton = document.querySelector("#minimize_button");
-
 minimizeButton.addEventListener("click", minimizeMenu);
 
 function minimizeMenu() {
-    var wrapper = document.querySelector("#wrapper")
-    var menu = document.querySelector("#menu")
-    menu.classList.toggle("minimized")
-    wrapper.classList.toggle("minimized")
+    var wrapper = document.querySelector("#wrapper");
+    var menu = document.querySelector("#menu");
+    menu.classList.remove("show_mobile");
+    menu.classList.toggle("minimized");
+    wrapper.classList.toggle("minimized");
+}
+
+var moreButton = document.querySelector("#mer");
+var closeButton = document.querySelector("#close");
+moreButton.addEventListener("click", showMobile);
+closeButton.addEventListener("click", showMobile);
+
+function showMobile() {
+    var menu = document.querySelector("#menu");
+    var wrapper = document.querySelector("#wrapper");
+    menu.classList.remove("minimized");
+    wrapper.classList.remove("minimized");
+    menu.classList.toggle("show_mobile");
 }
